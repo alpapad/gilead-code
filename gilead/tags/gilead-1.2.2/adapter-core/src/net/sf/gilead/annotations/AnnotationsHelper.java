2739ca9b5cdac8e36241cc8fcf30fe0c83873bfa
SomeOne__ <SomeOne__@8cb42d02-3994-4270-bb0b-85ab9ff68958>
2009-03-15 17:04:09 +0000

    [maven-release-plugin]  copy for tag gilead-1.2.2

---
+++ gilead/tags/gilead-1.2.2/adapter-core/src/net/sf/gilead/annotations/AnnotationsHelper.java
@@ -0,0 +1,285 @@
+package net.sf.gilead.annotations;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import net.sf.gilead.util.IntrospectionHelper;
+
+/**
+ * Helper class for annotation management
+ * @author bruno.marchesson
+ *
+ */
+public class AnnotationsHelper
+{
+	//----
+	// Attributes
+	//----
+	/**
+	 * Log channel
+	 */
+	private static Log _log = LogFactory.getLog(AnnotationsHelper.class);
+	
+	/**
+	 * Annotation map.
+	 * It is filled with associated Gilead annotation for all classes and properties 
+	 * for performance purpose (computing it each time is very expensive)
+	 */
+	private static Map<Class<?>, Map<String, Class<?>>> _annotationMap = new HashMap<Class<?>, Map<String,Class<?>>>();
+	
+	//-------------------------------------------------------------------------
+	//
+	// Static helpers
+	//
+	//-------------------------------------------------------------------------
+	/**
+	 * Indicated if the argument has "ServerOnly" annotation on the named field.
+	 */
+	public static boolean isServerOnly(Class<?> clazz, String propertyName)
+	{
+	//	Map checking
+	//
+		Map<String, Class<?>> propertyAnnotations = _annotationMap.get(clazz);
+		if (propertyAnnotations == null)
+		{
+		//	Compute property annotations
+		//
+			propertyAnnotations = getGileadAnnotations(clazz);
+			synchronized (_annotationMap)
+			{
+				_annotationMap.put(clazz, propertyAnnotations);
+			}
+		}
+		
+	//	Does the map contains @ServerOnly annotation for the argument property ?
+	//
+		Class<?> annotation  = propertyAnnotations.get(propertyName);
+		return ((annotation != null) && (ServerOnly.class.equals(annotation)));
+	}
+	
+	/**
+	 * Indicated if the argument has "ReadOnly" annotation on the named field.
+	 */
+	public static boolean isReadOnly(Class<?> clazz, String propertyName)
+	{
+	//	Map checking
+	//
+		Map<String, Class<?>> propertyAnnotations = _annotationMap.get(clazz);
+		if (propertyAnnotations == null)
+		{
+		//	Compute property annotations
+		//
+			propertyAnnotations = getGileadAnnotations(clazz);
+			synchronized (_annotationMap)
+			{
+				_annotationMap.put(clazz, propertyAnnotations);
+			}
+		}
+		
+	//	Does the map contains @ReadOnly annotation for the argument property ?
+	//
+		Class<?> annotation  = propertyAnnotations.get(propertyName);
+		return ((annotation != null) && (ReadOnly.class.equals(annotation)));
+	}
+	
+	/**
+	 * Indicated if the argument has "ReadOnly" annotation on the named field.
+	 */
+	public static boolean isServerOrReadOnly(Class<?> clazz, String propertyName)
+	{
+	//	Map checking
+	//
+		Map<String, Class<?>> propertyAnnotations = _annotationMap.get(clazz);
+		if (propertyAnnotations == null)
+		{
+		//	Compute property annotations
+		//
+			propertyAnnotations = getGileadAnnotations(clazz);
+			synchronized (_annotationMap)
+			{
+				_annotationMap.put(clazz, propertyAnnotations);
+			}
+		}
+		
+	//	Does the map contains @ReadOnly annotation for the argument property ?
+	//
+		Class<?> annotation  = propertyAnnotations.get(propertyName);
+		return ((annotation != null) && (
+				(ReadOnly.class.equals(annotation) || ServerOnly.class.equals(annotation))));
+	}
+	
+	/**
+	 * Indicated if the argument has "ServerOnly" annotation on one of its field.
+	 */
+	public static boolean hasServerOnlyAnnotations(Class<?> clazz)
+	{
+	//	Map checking
+	//
+		Map<String, Class<?>> propertyAnnotations = _annotationMap.get(clazz);
+		if (propertyAnnotations == null)
+		{
+		//	Compute property annotations
+		//
+			propertyAnnotations = getGileadAnnotations(clazz);
+			synchronized (_annotationMap)
+			{
+				_annotationMap.put(clazz, propertyAnnotations);
+			}
+		}
+		
+	//	Does the map contains @ServerOnly annotation ?
+	//
+		return propertyAnnotations.containsValue(ServerOnly.class);
+	}
+	
+	
+	
+	/**
+	 * Indicated if the argument has "ServerOnly" annotation on one of its field.
+	 */
+	public static boolean hasServerOnlyOrReadOnlyAnnotations(Class<?> clazz)
+	{
+	//	Map checking
+	//
+		Map<String, Class<?>> propertyAnnotations = _annotationMap.get(clazz);
+		if (propertyAnnotations == null)
+		{
+		//	Compute property annotations
+		//
+			propertyAnnotations = getGileadAnnotations(clazz);
+			synchronized (_annotationMap)
+			{
+				_annotationMap.put(clazz, propertyAnnotations);
+			}
+		}
+		
+	//	Does the map contains @ServerOnly or @ReadOnly annotation ?
+	//
+		return ((propertyAnnotations.containsValue(ServerOnly.class)) ||
+				(propertyAnnotations.containsValue(ReadOnly.class)));
+	}
+	
+	/**
+	 * Indicated if the argument has "ReadOnly" annotation on one of its field.
+	 */
+	public static boolean hasReadOnlyAnnotations(Class<?> clazz)
+	{
+	//	Map checking
+	//
+		Map<String, Class<?>> propertyAnnotations = _annotationMap.get(clazz);
+		if (propertyAnnotations == null)
+		{
+		//	Compute property annotations
+		//
+			propertyAnnotations = getGileadAnnotations(clazz);
+			synchronized (_annotationMap)
+			{
+				_annotationMap.put(clazz, propertyAnnotations);
+			}
+		}
+		
+	//	Does the map contains @ServerOnly annotation ?
+	//
+		return propertyAnnotations.containsValue(ReadOnly.class);
+	}
+	
+	//-------------------------------------------------------------------------
+	//
+	// Internal methods
+	//
+	//--------------------------------------------------------------------------
+	/**
+	 * Indicated if the argument has "ReadOnly" annotation on one of its field.
+	 */
+	private static Map<String,Class<?>> getGileadAnnotations(Class<?> clazz)
+	{
+		if (_log.isDebugEnabled())
+		{
+			_log.debug("Looking for Gilead annotations for " + clazz);
+		}
+		
+		Map<String, Class<?>> result = new HashMap<String, Class<?>>();
+		
+	//	Search annotations on fields
+	//
+		Field[] fields = IntrospectionHelper.getRecursiveDeclaredFields(clazz);
+		for (Field field : fields)
+		{
+			String propertyName = field.getName();
+			if (field.isAnnotationPresent(ReadOnly.class))
+			{
+				if (_log.isDebugEnabled())
+				{
+					_log.debug(propertyName + " member has @ReadOnly");
+				}
+				result.put(propertyName, ReadOnly.class);
+			}
+			else if (field.isAnnotationPresent(ServerOnly.class))
+			{
+				if (_log.isDebugEnabled())
+				{
+					_log.debug(propertyName + " member has @ServerOnly");
+				}
+				result.put(propertyName, ServerOnly.class);
+			}
+			else
+			{
+				if (_log.isTraceEnabled())
+				{
+					_log.trace(propertyName + " member has no Gilead annotation");
+				}
+				
+				result.put(propertyName, null);
+			}
+		}
+		
+	//	Search annotation on getters
+	//
+		try 
+		{
+			BeanInfo info = Introspector.getBeanInfo(clazz);
+			PropertyDescriptor[] descriptors = info.getPropertyDescriptors();
+			for (int index = 0; index < descriptors.length; index++)
+			{
+				PropertyDescriptor descriptor = descriptors[index];
+				if ((descriptor != null) && (descriptor.getReadMethod() != null))
+				{
+					String propertyName = descriptor.getName();
+					if (descriptor.getReadMethod().isAnnotationPresent(ReadOnly.class))
+					{
+						if (_log.isDebugEnabled())
+						{
+							_log.debug(propertyName + " getter has @ReadOnly");
+						}
+						result.put(propertyName, ReadOnly.class);
+					}
+					else if (descriptor.getReadMethod().isAnnotationPresent(ServerOnly.class))
+					{
+						if (_log.isDebugEnabled())
+						{
+							_log.debug(propertyName + " getter has @ServerOnly");
+						}
+						result.put(propertyName, ServerOnly.class);
+					}
+				}
+			}
+		} 
+		catch (IntrospectionException e)
+		{
+			throw new RuntimeException("Cannot inspect class " + clazz, e);
+		}
+		
+	//	Return annotation map
+	//
+		return result;
+	}
+	
+}
---
+++ gilead/tags/gilead-1.2.2/adapter-test/src/net/sf/gilead/core/serialization/SerializationManagerTest.java
@@ -0,0 +1,173 @@
+/**
+ * 
+ */
+package net.sf.gilead.core.serialization;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Test serialization manager behavior
+ * @author bruno.marchesson
+ *
+ */
+public class SerializationManagerTest extends TestCase
+{
+	//----
+	// Log channel
+	//----
+	/**
+	 * Log channel
+	 */
+	private static Log _log = LogFactory.getLog(SerializationManagerTest.class);
+	
+	//-------------------------------------------------------------------------
+	//
+	// Public test methods
+	//
+	//-------------------------------------------------------------------------
+	/**
+	 * Test Java serialization
+	 */
+	public void testByteStringProxySerialization()
+	{
+		IProxySerialization proxySerialization = new ByteStringProxySerialization();
+		testIntegerConversion(proxySerialization);
+		testLongConversion(proxySerialization);
+		testMapConversion(proxySerialization);
+	}
+	
+	/**
+	 * Test XStream serialization
+	 */
+	public void testXStreamProxySerialization()
+	{
+		IProxySerialization proxySerialization = new XStreamProxySerialization();
+		testIntegerConversion(proxySerialization);
+		testLongConversion(proxySerialization);
+		testMapConversion(proxySerialization);
+	}
+	
+	/**
+	 * Test JBoss serialization
+	 */
+	public void testJBossProxySerialization()
+	{
+		IProxySerialization proxySerialization = new JBossProxySerialization();
+		testIntegerConversion(proxySerialization);
+		testLongConversion(proxySerialization);
+		testMapConversion(proxySerialization);
+	}
+	
+	//-------------------------------------------------------------------------
+	//
+	// Internal Test methods
+	//
+	//-------------------------------------------------------------------------
+	/**
+	 * Test Integer convertor
+	 */
+	protected void testIntegerConversion(IProxySerialization proxySerialization)
+	{
+	//	Integer conversion
+	//
+		Integer value = new Integer(1);
+		long start = System.currentTimeMillis();
+		String serialized = proxySerialization.serialize(value);
+		long serialization = System.currentTimeMillis();
+		
+		assertNotNull(serialized);
+		assertEquals(value, proxySerialization.unserialize(serialized));
+		long end = System.currentTimeMillis();
+		
+		_log.info("Integer serialization took [" + (serialization-start) + ", " + (end-serialization) + "] ms");
+		
+	//	int conversion
+	//
+		int intValue = 1;
+		start = System.currentTimeMillis();
+		serialized = proxySerialization.serialize(intValue);
+		serialization = System.currentTimeMillis();
+		
+		assertNotNull(serialized);
+		assertEquals(intValue, proxySerialization.unserialize(serialized));
+		end = System.currentTimeMillis();
+		
+		_log.info("int serialization took [" + (serialization-start) + ", " + (end-serialization) + "] ms");
+	}
+	
+	/**
+	 * Test Long convertor
+	 */
+	protected void testLongConversion(IProxySerialization proxySerialization)
+	{
+	//	Long conversion
+	//
+		Long value = new Long(1);
+		long start = System.currentTimeMillis();
+		String serialized = proxySerialization.serialize(value);
+		long serialization = System.currentTimeMillis();
+		
+		assertNotNull(serialized);
+		assertEquals(value, proxySerialization.unserialize(serialized));
+		long end = System.currentTimeMillis();
+		
+		_log.info("Long serialization took [" + (serialization-start) + ", " + (end-serialization) + "] ms");
+		
+	//	long conversion
+	//
+		long longValue = 1;
+		start = System.currentTimeMillis();
+		serialized = proxySerialization.serialize(longValue);
+		serialization = System.currentTimeMillis();
+		
+		assertNotNull(serialized);
+		assertEquals(longValue, proxySerialization.unserialize(serialized));
+		end = System.currentTimeMillis();
+		
+		_log.info("long serialization took [" + (serialization-start) + ", " + (end-serialization) + "] ms");
+	}
+	
+	/**
+	 * Test Map conversion
+	 */
+	protected void testMapConversion(IProxySerialization proxySerialization)
+	{
+	//	Map creation
+	//
+		HashMap<String, Serializable> map = new HashMap<String, Serializable>();
+		map.put("id", 1);
+		map.put("initialized", true);
+		ArrayList<Integer> idList = new ArrayList<Integer>();
+		idList.add(2);
+		idList.add(3);
+		idList.add(4);
+		map.put("idList", idList);
+		
+	//	Test conversion
+	//
+		long start = System.currentTimeMillis();
+		String serialized = proxySerialization.serialize(map);
+		long serialization = System.currentTimeMillis();
+		
+		assertNotNull(serialized);
+		HashMap<String, Serializable> unserialized = (HashMap<String, Serializable>)
+													 proxySerialization.unserialize(serialized);
+		long end = System.currentTimeMillis();
+		
+		_log.info("Map serialization took [" + (serialization-start) + ", " + (end-serialization) + "] ms");
+		
+	//	Map checking
+	//
+		assertNotNull(unserialized);
+		assertEquals(map.size(), unserialized.size());
+		assertNotNull(unserialized.get("idList"));
+		assertEquals(3, ((ArrayList<Integer>)unserialized.get("idList")).size());
+	}
+}
